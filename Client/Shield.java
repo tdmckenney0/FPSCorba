6301f86143d4a23ebccc46fa1579eae932762b53
tmckenney <tmckenney7@outlook.com>
2014-05-19 00:55:32 -0400

    Completed Classes. Added Test Class.
    Completed Classes. Added Test Class.

--- Client/Shield.java
+++ Client/Shield.java
@@ -6,54 +6,92 @@ Shield Class
 
 */
 
-package Client;
-
 import java.util.Random; 
 
 public class Shield {
 
-	private String name; 			/* name of the shield */
-	private WeaponType type; 		/* WeaponType reflects */
-	private Integer resistance;		/* base resistance of the shield */
-	private Integer maxLevel; 		/* Ceiling the level can be set at. */
-	private Integer level;			/* Current level of the weapon; cannot be greater than maxLevel; randomly generated */
-	private Integer splashResist;	/* Resist damage afflicted randomly to players */
-	private Double rarity; 			/* Percent chance a weapon will appear in the pool */
+	/* Properties */
+
+		private String name; 			/* name of the shield */
+		private WeaponType type; 		/* WeaponType reflects */
+		private Integer resistance;		/* base resistance of the shield */
+		private Integer maxLevel; 		/* Ceiling the level can be set at. */
+		private Integer level;			/* Current level of the weapon; cannot be greater than maxLevel; randomly generated */
+		private Integer splashResist;	/* Resist damage afflicted randomly to players */
+		private Double rarity; 			/* Percent chance a weapon will appear in the pool */
 	
-	public Shield(String name, WeaponType type, Integer resistance, Integer maxLevel, Integer splashResist, Double rarity) {
+	/* Constructor */
 	
-		Random rand = new Random(); 
-	
-		this.name = name;
-		this.type = type;
-		this.maxLevel = maxLevel;
-		this.rarity = rarity;
+		public Shield(String name, WeaponType type, Integer resistance, Integer maxLevel, Integer splashResist, Double rarity) {
 		
-		this.level = (rand.nextInt(this.maxLevel) + 1); // 0 Is inclusive and maxLevel is exclusive, so shift right by one. 
+			Random rand = new Random(); 
 		
-		this.resistance = (resistance * this.level); // calculate the resistance
-		this.splashResist = (splashResist * this.level); // calculate the splash
-	}
+			this.name = name;
+			this.type = type;
+			this.maxLevel = maxLevel;
+			this.rarity = rarity;
+			
+			this.level = (rand.nextInt(this.maxLevel) + 1); // 0 Is inclusive and maxLevel is exclusive, so shift right by one. 
+			
+			this.resistance = (resistance * this.level); // calculate the resistance
+			this.splashResist = (splashResist * this.level); // calculate the splash
+			
+			// Shitty way to do this. But Pressed for time. 
+			
+			switch(this.level) {
+				case 1:
+					this.name = name + " I";
+				break; 
+				
+				case 2:
+					this.name = name + " II";
+				break; 
+				
+				case 3:
+					this.name = name + " III";
+				break; 
+				
+				case 4:
+					this.name = name + " IV";
+				break; 
+				
+				case 5:
+					this.name = name + " V";
+				break; 
+				
+				default:
+					// You shouldn't be here. 
+				break;
+			}
+		}
 	
-	/* Getters and Setters */
+	/* Getters */
 	
-	public String getName() {
-		return this.name; 
-	}
-	
-	public WeaponType getType() {
-		return this.type; 
-	}
-	
-	public Integer getLevel() {
-		return this.level; 
-	}
-	
-	public Integer getResistance() {
-		return this.resistance; 
-	}
-	
-	public Integer getSplashResistance() {
-		return this.splashResist; 
-	}
+		public String getName() {
+			return this.name; 
+		}
+		
+		public WeaponType getType() {
+			return this.type; 
+		}
+		
+		public Integer getResistance() {
+			return this.resistance; 
+		}
+		
+		public Integer getLevel() {
+			return this.level; 
+		}
+
+		public Integer getSplashResistance() {
+			return this.splashResist; 
+		}
+		
+		public Double getRarity() {
+			return this.rarity; 
+		}	 
+		
+		public String toString() {
+			return this.getName();
+		}
 }
--- Client/Weapon.java
+++ Client/Weapon.java
@@ -6,48 +6,93 @@
 	
 */
 
-package Client;
-
 import java.util.Random; 
 
 public class Weapon {
 
-	private String name; 			/* name of the weapon */
-	private WeaponType type; 		/* WeaponType determine */
-	private Integer damage;		/* base Damage of the weapon */
-	private Integer maxLevel; 		/* Ceiling the level can be set at. */
-	private Integer level;			/* Current level of the weapon; cannot be greater than maxLevel; randomly generated */
-	private Integer splashDamage;	/* Damage afflicted randomly to other players */
-	private Double rarity; 			/* Percent chance a weapon will appear in the pool */
-	
+	/* Properties */
 
-	public Weapon(String name, WeaponType type, Integer damage, Integer maxLevel, Integer splashDamage, Double rarity) {
-	
-		Random rand = new Random(); 
-	
-		this.name = name;
-		this.type = type;
-		this.maxLevel = maxLevel;
-		this.rarity = rarity;
+		private String name; 			/* name of the weapon */
+		private WeaponType type; 		/* WeaponType determine */
+		private Integer damage;		/* base Damage of the weapon */
+		private Integer maxLevel; 		/* Ceiling the level can be set at. */
+		private Integer level;			/* Current level of the weapon; cannot be greater than maxLevel; randomly generated */
+		private Integer splashDamage;	/* Damage afflicted randomly to other players */
+		private Double rarity; 			/* Percent chance a weapon will appear in the pool */
 		
-		this.level = (rand.nextInt(this.maxLevel) + 1); // 0 Is inclusive and maxLevel is exclusive, so shift right by one. 
+	/* Constructor */
+	
+		public Weapon(String name, WeaponType type, Integer damage, Integer maxLevel, Integer splashDamage, Double rarity) {
 		
-		this.damage = (damage * this.level); // calculate the damage
-		this.splashDamage = (splashDamage * this.level); // calculate the splash
-	}
+			Random rand = new Random(); 
+		
+			this.name = name;
+			this.type = type;
+			this.maxLevel = maxLevel;
+			this.rarity = rarity;
+			
+			this.level = (rand.nextInt(this.maxLevel) + 1); // 0 Is inclusive and maxLevel is exclusive, so shift right by one. 
+			
+			this.damage = (damage * this.level); // calculate the damage
+			this.splashDamage = (splashDamage * this.level); // calculate the splash
+			
+			// Shitty way to do this. But Pressed for time. 
+			
+			switch(this.level) {
+				case 1:
+					this.name = name + " I";
+				break; 
+				
+				case 2:
+					this.name = name + " II";
+				break; 
+				
+				case 3:
+					this.name = name + " III";
+				break; 
+				
+				case 4:
+					this.name = name + " IV";
+				break; 
+				
+				case 5:
+					this.name = name + " V";
+				break; 
+				
+				default:
+					// You shouldn't be here. 
+				break;
+			}
+		}
 
-	/* Getters and Setters */
+	/* Getters */
 	
-	public String getName() {
-		return this.name; 
-	}
+		public String getName() {
+			return this.name; 
+		}
+		
+		public WeaponType getType() {
+			return this.type; 
+		}
+		
+		public Integer getDamage() {
+			return this.damage; 
+		}
+		
+		public Integer getLevel() {
+			return this.level; 
+		}
+
+		public Integer getSplashDamage() {
+			return this.splashDamage; 
+		}
+		
+		public Double getRarity() {
+			return this.rarity; 
+		}
 	
-	public WeaponType getType() {
-		return this.type; 
-	}
-	
-	public Integer getLevel() {
-		return this.level; 
-	}
-	
+		public String toString() {
+			return this.getName();
+		}
+
 }
